e2a845fe209c30e2236b71a8b0ea923c7dab6731
tom <tom.errington58@gmail.com>
2021-04-14 17:00:53 -0400

    Created insert into db func

---
+++ canned_query.sql
@@ -0,0 +1,22 @@
+-- SQLite
+CREATE TABLE IF NOT EXISTS ADDRESS (
+    symbol_1 TEXT NOT NULL,
+    symbol_2 TEXT NOT NULL,
+    symbol_3 TEXT NOT NULL,
+    symbol_4 TEXT NOT NULL,
+    symbol_5 TEXT NOT NULL,
+    symbol_6 TEXT NOT NULL,
+    symbol_7 TEXT NOT NULL,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    CONSTRAINT con UNIQUE (symbol_1,symbol_2,symbol_3,symbol_4,symbol_5,symbol_6,symbol_7)  
+);
+drop table ADDRESS
+select * from ADDRESS
+
+delete from ADDRESS where symbol_1 = 'sol2'
+
+
+
+
+
+SELECT * FROM ADDRESS WHERE symbol_1 = 'sol' AND symbol_2 = 'vega' AND symbol_3 = 'siruis' AND symbol_4 = 'betelgeuse' AND symbol_5 = 'polaris' AND symbol_6 = 'mira' AND symbol_7 = 'alpha centauri A';
--- ring_sim_db.py
+++ ring_sim_db.py
@@ -1,5 +1,5 @@
 import sqlite3
-from sqlite3.dbapi2 import PARSE_COLNAMES
+from sqlite3.dbapi2 import PARSE_COLNAMES, register_adapter
 
 class DBConnection():
 
@@ -15,6 +15,7 @@ class DBConnection():
     def __init__(self, db_name='ringsim.db'):
         self.name = db_name
         self.conn = self.connect()
+        self.cursor = self.conn.cursor()
 
     def connect(self):
         try:
@@ -26,37 +27,34 @@ class DBConnection():
   # write CRUD operations below
 
     # Record CRUD
-    def create_record(self, table):
-        self.conn.execute("INSERT INTO {}",table)
+    def create_record(self, address):
+        values = address.get_complete_address()
+        query = '''INSERT INTO ADDRESS 
+                    (symbol_1, symbol_2, symbol_3, symbol_4, symbol_5, symbol_6, symbol_7) 
+                    VALUES(?,?,?,?,?,?,?);'''
+        self.cursor.execute(query,values)
+        self.conn.commit()
+
+    def read_record(self, address):
+        values = address.get_complete_address()
+        query = "SELECT * FROM ADDRESS WHERE"
+        where = f''' symbol_1 = '{values[0]}' AND symbol_2 = '{values[1]}' AND symbol_3 = '{values[2]}' AND symbol_4 = '{values[3]}' AND symbol_5 = '{values[4]}' AND symbol_6 = '{values[5]}' AND symbol_7 = '{values[6]}';''' 
+                    #.format(values[0],values[1],values[2],values[3],values[4],values[5],values[6])
+        
+        query = query + where
+
+        address2 = self.cursor.execute(query)
+        #print(address2.fetchone())
+        self.conn.commit()
+
+        return address2
+
+
+    def update_record(self, table, address):
+        self.conn.execute("INSERT INTO {} WHERE self.conn.name = {}", address)
         pass
 
-    def read_record(self, table):
-        self.conn.execute("SELECT * FROM {}", table)
+    def delete_record(self, table, address):
+        self.conn.execute("DROP FROM TABLE {} WHERE self.conn.name = {}", address)
         pass
 
-    def update_record(self, table, record):
-        self.conn.execute("INSERT INTO {} WHERE self.conn.name = {}", table, record)
-        pass
-
-    def delete_record(self, table, record):
-        self.conn.execute("DROP FROM TABLE {} WHERE self.conn.name = {} ", table, record)
-        pass
-
-    # Table CRUD
-    def create_table(self, table):
-        self.conn()
-        pass
-
-    def read_table(self, table):
-        pass
-
-    def update_table(self, table):
-        # this might need more defining of what type of modification is desired
-        pass
-
-    def delete_table(self, table):
-        pass
-
-    # Run any custom query that is desired
-    def custom_query(self, table):
-        pass
